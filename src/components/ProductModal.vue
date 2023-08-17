df2302a2fd36cfe84c300857cfa35272e65fbc53
MSI\nonwhen07 <nonwhen07@gmail.com>
2023-08-17 16:32:48 +0800

    showModal

---
+++ src/components/ProductModal.vue
@@ -0,0 +1,48 @@
+<template>
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"
+    ref="modal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// 因為這部分元件只需要bootstrap的modal方法，所以不需要全部載出來
+import Modal from 'bootstrap/js/dist/modal'
+
+export default {
+  data () {
+    return {
+      modal: {}
+    }
+  },
+  methods: {
+    showModal () {
+      this.modal.show()
+    },
+    hideModal () {
+      this.modal.hide()
+    }
+  },
+  mounted () {
+    // var myModal = new bootstrap.Modal(document.getElementById('myModal'), options)
+    // 指向外層的 ref dom 元素
+    this.modal = new Modal(this.$refs.modal)
+  }
+
+}
+</script>
